22cca686f5314e9900b36a44bb994fe37f7b1de8
jjyoonki <yoonki.choi@gmail.com>
2021-12-05 17:50:47 +0900

    C1

---
+++ code.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+	var n=1;
+	while(1){
+		var obj_code = document.getElementById('code'+n);
+		var obj_js   = document.getElementById('js'+n);
+		if( obj_code==undefined || obj_js==undefined )
+			break;
+		obj_code.innerHTML = obj_js.innerHTML.slice(1);
+		n++;
+	}
+}
+
+function rainbow_code(){
+	var n=1;
+	while(1){
+		var obj_code = document.getElementById('code'+n);
+		var obj_js   = document.getElementById('js'+n);
+		if( obj_code==undefined || obj_js==undefined )
+			break;
+		obj_code.innerHTML = obj_js.innerHTML.slice(1);
+		n++;
+	}
+}
